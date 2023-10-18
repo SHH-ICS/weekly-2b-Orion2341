4a33b6077aec7bacd508e034a7e8c79173c811af
Orion2341 <144241864+Orion2341@users.noreply.github.com>
2023-10-18 13:19:23 +0000

    added texts

--- assignment.py
+++ assignment.py
@@ -1,1 +1,9 @@
-
+import math 
+txt = "Hello, welcome to circle solver!"
+txt2 = "Area of a circle"
+txt3 = "Circumference of a circle"
+x = txt.center(80)
+y = txt2.center(20)
+z = txt3.center(100)
+print(x)
+print(y, z)

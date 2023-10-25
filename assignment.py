636bebf99959b62cad4f3e5656f844f00cb2f960
Orion2341 <144241864+Orion2341@users.noreply.github.com>
2023-10-25 00:52:39 +0000

    added code

--- assignment.py
+++ assignment.py
@@ -1,9 +1,7 @@
 import math 
-txt = "Hello, welcome to circle solver!"
 radius = float(input("Enter the radius"))
 circumference = 2*math.pi*radius
 area = math.pi*radius**2
-x = txt.center(80)
-print(x)
+print("Hello, welcome to circle solver!")
 print("Circumference =", circumference)
 print("Area =", area)

27186d597deda9caedded2918096a9d9ab858063
Orion2341 <144241864+Orion2341@users.noreply.github.com>
2023-10-25 01:25:29 +0000

    added code

--- assignment.py
+++ assignment.py
@@ -6,3 +6,4 @@ area = math.pi*radius**2
 print(":")
 print("Circumference =", circumference)
 print("Area =", area)
+#quick ps, decided to merge both my area and circumference together instead of having to do it one at a time to make it easier to code and easier for the user, that's all :)

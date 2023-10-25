bd4cf8482cdd57ba4fd207669ea02efc5887becd
Orion2341 <144241864+Orion2341@users.noreply.github.com>
2023-10-25 01:23:04 +0000

    added code

--- assignment.php
+++ assignment.php
@@ -1,7 +1,15 @@
-<?php
-$radius = floatval(readline("Enter Radius"));
-$circumference = 2*pi()*$radius;
-$area = pi()*pow($radius, 2);
-echo "Circumference =" . $circumference . "\n";
-echo "Area =" . $area . "\n";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+            <h1>Hello, welcome to circle solver!</h1>
+    </head>
+    <body>
+    <?php
+        $radius = floatval(readline("Enter Radius"));
+        $circumference = 2*pi()*$radius;
+        $area = pi()*pow($radius, 2);
+        echo "Circumference =" . $circumference . "\n";
+        echo "Area =" . $area . "\n";
+    ?>
+    </body>
+</html>
